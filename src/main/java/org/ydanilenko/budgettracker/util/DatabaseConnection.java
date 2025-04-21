e7309b1a7b3ad9cb9a87c165c0ee8e708d4507e1
yehord <yehordanilenko@gmail.com>
2025-04-21 18:06:11 +0200

    tests

--- src/main/java/org/ydanilenko/budgettracker/util/DatabaseConnection.java
+++ src/main/java/org/ydanilenko/budgettracker/util/DatabaseConnection.java
@@ -1,21 +1,34 @@
 package org.ydanilenko.budgettracker.util;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DatabaseConnection {
-    private static final String URL =
-            "jdbc:mysql://localhost:3306/bta?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
-    private static final String USER = "root";
-    private static final String PASSWORD = "12345qwert";
+    private static final String PROPERTIES_FILE = "/config.properties";
 
     public static Connection getConnection() {
-        try {
-            return DriverManager.getConnection(URL, USER, PASSWORD);
+        try (InputStream input = DatabaseConnection.class.getResourceAsStream(PROPERTIES_FILE)) {
+            if (input == null) {
+                throw new RuntimeException("Unable to find config.properties");
+            }
+
+            Properties props = new Properties();
+            props.load(input);
+
+            String url = props.getProperty("db.url");
+            String user = props.getProperty("db.user");
+            String password = props.getProperty("db.password");
+
+            return DriverManager.getConnection(url, user, password);
+
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load database configuration", e);
         }
     }
 }
